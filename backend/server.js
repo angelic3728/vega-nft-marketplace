6f90f2ce5a2806371c424bead71eeb990c681853
dream-builder48 <superstar200720@gmail.com>
2022-02-11 19:00:24 +0300

    setting for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const cors = require("cors");
-const HttpException = require('./utils/HttpException.utils');
-const errorMiddleware = require('./middleware/error.middleware');
-const router = require('./route');
+const HttpException = require("./utils/HttpException.utils");
+const errorMiddleware = require("./middleware/error.middleware");
+const router = require("./route");
 
 // Init express
 const app = express();
@@ -13,6 +13,10 @@ dotenv.config();
 // parses incoming requests with JSON payloads
 app.use(express.json());
 // enabling cors for all requests by using cors middleware
+
+// for deployment
+app.use(express.static(path.join(__dirname, "../frontend", "build")));
+
 app.use(cors());
 // Enable pre-flight
 app.options("*", cors());
@@ -22,17 +26,20 @@ const port = Number(process.env.PORT || 5000);
 app.use(`/vega`, router);
 
 // 404 error
-app.all('*', (req, res, next) => {
-    const err = new HttpException(404, 'Endpoint Not Found');
-    next(err);
+app.all("*", (req, res, next) => {
+  const err = new HttpException(404, "Endpoint Not Found");
+  next(err);
 });
 
 // Error middleware
 app.use(errorMiddleware);
 
-// starting the server
-app.listen(port, () =>
-    console.log(`🚀 Server running on port ${port}!`));
+// for deployment
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+});
 
+// starting the server
+app.listen(port, () => console.log(`🚀 Server running on port ${port}!`));
 
 module.exports = app;
